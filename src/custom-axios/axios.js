b0c1945fd452ef84132c76149c9eb33448a4bc5e
mpavlovska <marijapavlovska01@gmail.com>
2022-04-25 13:17:36 +0200

    Updated

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9191/api',
+    baseURL: 'https://emtlibrarybackend.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
